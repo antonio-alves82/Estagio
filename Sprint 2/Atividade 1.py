18d3157fd9905478b0c41b524abeda5eb82ca040
caiquemain <caiquemain@gmail.com>
2022-04-11 11:44:37 -0300

    Atividades 1 e 2

---
+++ Sprint 2/Atividade 1.py
@@ -0,0 +1,6 @@
+print ('Qual é a sua idade ?')
+idade = int(input())
+if idade < 100: 
+    print ('falta ' + str(100 - idade) + ' anos para você completar 100 anos')
+else:
+    print ('você possui mais de 100 anos')
---
+++ Sprint 2/Atividade 2.py
@@ -0,0 +1,8 @@
+print ('Digite um número')
+numero = int (input())
+resultado = numero % 2
+
+if resultado == 0: 
+    print ('Esse número ' + str(numero) + ' é par')
+else:
+    print ('Esse número ' + str(numero) + ' é impar' )
