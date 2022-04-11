a5a04743e896215b750a3cf685590e55029d6f62
caiquemain <caiquemain@gmail.com>
2022-04-11 13:48:10 -0300

    Comentando os códigos

--- Sprint 2/Atividade 1 pt1.py
+++ Sprint 2/Atividade 1 pt1.py
@@ -1,5 +1,7 @@
 print ('Qual é a sua idade ?')
+#pegando a informação da idade
 idade = int(input())
+#verificando se a idade é maior que 100 anos ou não
 if idade < 100: 
     print ('falta ' + str(100 - idade) + ' anos para você completar 100 anos')
 else:
--- Sprint 2/Atividade 2 pt 1.py
+++ Sprint 2/Atividade 2 pt 1.py
@@ -1,7 +1,8 @@
+#pegando o valor de um número qualquer
 print ('Digite um número')
 numero = int (input())
+#calculo para saber se a divisão termina em 0 ou 1
 resultado = numero % 2
-
 if resultado == 0: 
     print ('Esse número ' + str(numero) + ' é par')
 else:
