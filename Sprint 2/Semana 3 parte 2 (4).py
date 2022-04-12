5a9e2821ae6290f1af53fa3f4408c38a9a14ac2f
caiquemain <caiquemain@gmail.com>
2022-04-12 19:30:50 -0300

    Atividades e comentários nos arquivos

---
+++ Sprint 2/Semana 3 parte 2 (4).py
@@ -0,0 +1,10 @@
+from itertools import chain
+
+primeirosNomes = ['Joao', 'Douglas', 'Lucas', 'José']
+sobreNomes = ['Soares', 'Souza', 'Silveira', 'Pedreira']
+idades = [19, 28, 25, 31]
+
+juntos = list(chain(primeirosNomes, sobreNomes, idades))
+
+frase = ''.join(juntos)
+print = ("Teste de texto ", frase)
