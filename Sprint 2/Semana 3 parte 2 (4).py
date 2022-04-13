64c3b3625b75bbe11a57c0d10ca8aba21c52632c
caiquemain <caiquemain@gmail.com>
2022-04-13 09:18:14 -0300

    Alterações finais

--- Sprint 2/Semana 3 parte 2 (4).py
+++ Sprint 2/Semana 3 parte 2 (4).py
@@ -1,10 +1,8 @@
-from itertools import chain
-
 primeirosNomes = ['Joao', 'Douglas', 'Lucas', 'José']
 sobreNomes = ['Soares', 'Souza', 'Silveira', 'Pedreira']
 idades = [19, 28, 25, 31]
 
-juntos = list(chain(primeirosNomes, sobreNomes, idades))
-
-frase = ''.join(juntos)
-print = (frase)
+for i in range(len(primeirosNomes)):
+    print("{} {} está com {} anos.".format(primeirosNomes[i], sobreNomes[i], idades[i]))
+import os
+os.system("pause")
