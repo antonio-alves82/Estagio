6d43ed5f967b548a2898bcab1e1f3ea81fbaa2aa
caiquemain <caiquemain@gmail.com>
2022-04-19 11:19:39 -0300

    Atividade 2

--- Sprint 2/Semana 4/Desafio/teste.py
+++ Sprint 2/Semana 4/Desafio/teste.py
@@ -1,5 +1,8 @@
 import csv
 
+from nbformat import write
+
+
 with open('actors.csv', 'r') as actors_csv:
     leitor = csv.reader(actors_csv)
     next(leitor)
@@ -9,10 +12,23 @@ with open('actors.csv', 'r') as actors_csv:
 with open('Atividade_1.txt', 'w') as a1:
     a1.write(str1)
 
-'''with open('actors.csv', 'r') as actors_csv:
-    leitor = csv.reader(actors_csv)
-    next(leitor)
-    resposta '''
+with open('actors.csv', 'r') as actors_csv:
+    leitor = csv.DictReader(actors_csv)
+    Actor = []
+    nMoveis = []
+
+    for col in leitor:
+        Actor.append(col['Actor'])
+        nMoveis.append(col['Number of Movies'])
+    print('Actor: ', Actor)
+    print('Numero de filmes: ', nMoveis)
+    ZipResposta2 = zip(Actor, nMoveis)
+    Resposta2 = list(ZipResposta2)
+    print(Resposta2)
+    str2 = '|'.join(map(str, Resposta2))
+with open('Atividade_2.txt', 'w') as a2:
+    a2.write(str2)
+
 
 with open('actors.csv', 'r') as actors_csv:
     leitor = csv.reader(actors_csv)
